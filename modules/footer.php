4512eb8790595fa324ef8724a910635ebfa8cb72
Gaurav verma <94301843+abcdeCoder@users.noreply.github.com>
2022-05-10 23:06:11 +0530

    Student_Attendance

---
+++ modules/footer.php
@@ -0,0 +1,52 @@
+</div>
+<footer id="footer">
+    <div id="">
+
+
+
+
+        </p>
+    </div>
+    <div id="links">
+        Developers:
+        <a href="https://abcdecoder.github.io/portfolio.github.io/" taget="_blank">Gaurav Verma </a>
+        <a href="https://github.com/abcdeCoder" taget="_blank">Follow Me on Github</a>
+        <a href="https://www.linkedin.com/in/gaurav-verma-88056a224/" taget="_blank"> Connect Form Me on Linkedin</a>
+        <a href="mailto:gaurav.02116403220@ipu.ac.in"><img src="profile.jpg" alt="" height="80px"></a>
+        &copy; Copyright 2022Gaurav Verma
+
+    </div>
+
+</footer>
+<style>
+footer {
+    position: fixed;
+    bottom: 0;
+    background-color: palevioletred;
+    width: 100%;
+    text-align: center;
+    color: black;
+    padding-bottom: 10px;
+    height: 13vh;
+
+}
+
+
+#footer a {
+    margin-right: 10px;
+    text-decoration: none;
+    color: green;
+    padding-left: 15px;
+    padding-right: 15px;
+    margin-bottom: 10px;
+}
+
+footer a:hover {
+    color: white;
+}
+</style>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/custom.js"></script>
+</body>
+
+</html>
